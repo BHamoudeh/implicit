97c7fe1bb907a75a8f69b6673377742f0142a6be
ethanrosenthal <ethanrosenthal@gmail.com>
2016-10-12 23:02:54 -0400

    Add ALS class

--- implicit/implicit.py
+++ implicit/implicit.py
@@ -8,6 +8,93 @@ from . import _implicit
 log = logging.getLogger("implicit")
 
 
+class ALS(object):
+
+    def __init__(self,
+                 num_factors=40,
+                 regularization=0.01,
+                 iterations=15,
+                 use_native=True,
+                 num_threads=0,
+                 dtype=np.float64):
+        """
+        Class version of alternating least squares implicit matrix factorization
+
+        Args:
+            num_factors (int): Number of factors to extract
+            regularization (double): Regularization parameter to use
+            iterations (int): Number of alternating least squares iterations to
+            run
+            use_native (bool): Whether or not to use Cython solver
+            num_threads (int): Number of threads to run least squares iterations.
+            0 means to use all CPU cores.
+            dtype (np dtype): Datatype for numpy arrays
+        """
+        self.num_factors = num_factors
+        self.regularization = regularization
+        self.iterations = iterations
+        self.use_native = use_native
+        self.num_threads = num_threads
+        self.dtype = dtype
+
+    def fit(self, Cui):
+        """
+        Fit an alternating least squares model on Cui data
+
+        Args:
+            Cui (sparse matrix, shape=(num_users, num_items)): Matrix of
+            user-item "interactions"
+        """
+
+        _check_open_blas()
+
+        users, items = Cui.shape
+
+        self.user_vectors = np.random.normal(size=(users, self.num_factors))\
+                                     .astype(self.dtype)
+
+        self.item_vectors = np.random.normal(size=(items, self.num_factors))\
+                                     .astype(self.dtype)
+
+        self.solver = _implicit.least_squares if self.use_native else least_squares
+
+        self.fit_partial(Cui)
+
+    def fit_partial(self, Cui):
+        """Continue fitting model"""
+
+        Cui, Ciu = Cui.tocsr(), Cui.T.tocsr()
+
+        for iteration in range(self.iterations):
+            s = time.time()
+            self.solver(Cui,
+                        self.user_vectors,
+                        self.item_vectors,
+                        self.regularization,
+                        self.num_threads)
+            self.solver(Ciu,
+                        self.item_vectors,
+                        self.user_vectors,
+                        self.regularization,
+                        self.num_threads)
+            log.debug("finished iteration %i in %s", iteration, time.time() - s)
+
+    def predict(self, user, item):
+        """Predict for single user and item"""
+        return self.user_vectors[user, :].dot(self.item_vectors[item, :].T)
+
+    def predict_for_customers(self,):
+        """Recommend products for all customers"""
+        return self.user_vectors.dot(self.item_vectors.T)
+
+    def predict_for_items(self, norm=True):
+        """Recommend products for all products"""
+        pred = self.item_vectors.dot(self.item_vectors.T)
+        if norm:
+            norms = np.array([np.sqrt(np.diagonal(pred))])
+            pred = pred / norms / norms.T
+        return pred
+
 def alternating_least_squares(Cui, factors, regularization=0.01,
                               iterations=15, use_native=True, num_threads=0,
                               dtype=np.float64):
